6a1c5f62274bf1542376ba3631eafa8e3e5df635
Sam McAfee <mcafee.sam@gmail.com>
2013-05-11 21:38:52 -0700

    same as before. but right this time. :)

---
+++ app/data.php
@@ -0,0 +1,13 @@
+<?php
+
+echo '[
+        {"id": 1, "name": "Ancient Egypt", "img" : "1ancient.png"},
+        {"id": 2, "name": "Ancient Greece", "img" : "2Medieval.jpg"},
+        {"id": 3, "name": "Medieval Europe", "img" : "3renaissmall.jpg"},
+        {"id": 4, "name": "Renaissance Europe", "img" : "4Discovery.jpg"},
+        {"id": 5, "name": "American Revolution", "img" : "5Enlightenment.jpg"},
+        {"id": 6, "name": "Nineteenth Century", "img" : "6Industrial.png"},
+        {"id": 7, "name": "Modern Era", "img" : "7Machine"}
+]';
+
+?>
